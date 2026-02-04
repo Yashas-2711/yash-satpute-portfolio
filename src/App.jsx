5b183def33b91c90162036a2ee4bdd7b45663a90
Yash Stpute <161969120+Yash-27112005@users.noreply.github.com>
2026-02-04 10:25:08 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
+            
           </Routes>
         
       </div>
